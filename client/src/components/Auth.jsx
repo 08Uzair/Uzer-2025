01b25d306ab9e7b553a54bc46b0ffd180b4b5fb0
08Uzer <uq0803@gmail.com>
2024-07-18 23:55:29 +0530

    changed the background of Auth Page

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -68,7 +68,7 @@ const Auth = () => {
     return <>Loading....</>;
   }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center ">
       <div className="bg-white p-8 shadow-md w-full max-w-md">
         <div className="flex justify-center mb-8">
           <button
